54d9088fae1430bbc22301368a193ceac7652bbf
KJ Osenenko <karljosenenko@gmail.com>
2022-10-01 20:50:38 -0600

    WIP Contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,21 @@
 class MessagesController < ApplicationController
   def new
-
+    @message = Message.new
   end
 
   def create
+    @message = Message.find_or_create_by(contact_email: message_params[:contact_email])
+    if @message.update(message_params)
+      redirect_to root_path
+      # redirect to a success path
+    else
+      # render a trubo frame
+      render :new
+    end
+  end
 
+  private
+  def message_params
+    params.require(:message).permit(:contact_name, :contact_email, :message_body, :message_subject, :contact_phone)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  validates_presence_of :contact_name
+  validates_presence_of :contact_email
+  validates_presence_of :message_body
+  validates_presence_of :message_subject
 end

9c0b305ca298ff08aac460d79e2cb7fcc5b59f89
KJ Osenenko <karljosenenko@gmail.com>
2022-10-08 12:24:17 -0600

    CRUD UI for Show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,7 @@
 class ShowsController < ApplicationController
+  protect_from_forgery prepend: true # Look into deleting after we add user auth.
+  before_action :get_show, only: [:edit, :update, :destroy]
+
   def index
     @shows = Show.current
   end
@@ -8,7 +11,13 @@ class ShowsController < ApplicationController
   end
 
   def create
-    @show = Show.create(show_params)
+    @show = Show.new(show_params)
+    if @show.save
+      redirect_to shows_path
+    else
+      flash[:errors] = @show.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -16,15 +25,25 @@ class ShowsController < ApplicationController
   end
 
   def update
-
+    if @show.update(show_params)
+      redirect_to shows_path
+    else
+      flash[:errors] = @show.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
-
+    @show.destroy
+    redirect_to shows_path
   end
 
   private
   def show_params
     params.require(:show).permit(:venue, :presale_link, :supporting_acts, :show_time, :show_date)
+  end
+
+  def get_show
+    @show=Show.find(params[:id])
   end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ApplicationRecord
+  validates_presence_of :venue
+  validates_presence_of :show_time
+  validates_presence_of :show_date
+
   scope :current, -> { where("show_date >= ?", Date.today) }
 end
